3fb635c64bd44d3992ec842f23fefcc1b7ce39f4
sertkayali <61178927+sertkayali@users.noreply.github.com>
2021-11-17 20:19:42 +0300

    Ders Sonu

--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -14,11 +15,11 @@ namespace DataAccess.Concrete.InMemory
         {
             _cars = new List<Car>()
             {
-                new Car() { BrandId = 1, ColorId = 1, DailyPrice = 100, Description = "Hyundai Accent", Id = 1, ModelYear = 2000},
-                new Car() { BrandId = 1, ColorId = 2, DailyPrice = 120, Description = "Hyundai Elantra", Id = 2, ModelYear = 2005},
-                new Car() { BrandId = 2, ColorId = 3, DailyPrice = 130, Description = "Ford Focus", Id = 3, ModelYear = 2008},
-                new Car() { BrandId = 2, ColorId = 4, DailyPrice = 150, Description = "Ford Mondeo", Id = 4, ModelYear = 2012},
-                new Car() { BrandId = 3, ColorId = 4, DailyPrice = 200, Description = "BMW 320d", Id = 5, ModelYear = 2017},
+                new Car() { BrandId = 1, ColorId = 1, DailyPrice = 100, Description = "ınmemoryHyundai Accent", Id = 1, ModelYear = 2000},
+                new Car() { BrandId = 1, ColorId = 2, DailyPrice = 120, Description = "ınmemoryHyundai Elantra", Id = 2, ModelYear = 2005},
+                new Car() { BrandId = 2, ColorId = 3, DailyPrice = 130, Description = "ınmemoryFord Focus", Id = 3, ModelYear = 2008},
+                new Car() { BrandId = 2, ColorId = 4, DailyPrice = 150, Description = "ınmemoryFord Mondeo", Id = 4, ModelYear = 2012},
+                new Car() { BrandId = 3, ColorId = 4, DailyPrice = 200, Description = "ınmemoryBMW 320d", Id = 5, ModelYear = 2017},
             };
         }
         public void Add(Car car)
@@ -32,9 +33,19 @@ namespace DataAccess.Concrete.InMemory
             _cars.Remove(carToDelete);
         }
 
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Car> GetAll()
         {
             return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Car> GetById(int Id)
